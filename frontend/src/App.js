f71743894e710a7f4dc1eca718ad834e0d67cf97
Anirudh A V <anirudh.av02@gmail.com>
2022-09-29 13:25:10 +0530

    frontend folder deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import logo from './logo.svg';
+import './App.css';
 
-import './App.scss'
-
-import { About, Footer, Header, Skills, Testimonials, Work } from './containers';
-import { Navbar } from './components';
-
-const App = () => {
+function App() {
   return (
-    <div className='App'>
-        <Navbar />
-        <Header />
-        <About />
-        <Skills />
-        <Work />
-        <Testimonials />
-        <Footer />
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export default App
+export default App;
