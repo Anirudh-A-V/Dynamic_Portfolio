8b38909b15520372af964dd92e960325672fefb4
Anirudh A V <anirudh.av02@gmail.com>
2022-09-30 00:22:17 +0530

    Basic structure of navbar designed

--- frontend-app/src/components/Navbar/Navbar.jsx
+++ frontend-app/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './Navbar.scss'
 
+import { images } from '../../constants'
+
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt='logo' />
       </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'contact', 'work', 'skills', 'projects'].map((item) => (
+          <li key={`link-${item}`} className='app__flex p-text'>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
