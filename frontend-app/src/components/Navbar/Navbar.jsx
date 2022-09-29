a71f7286aca89726f7a070adf981b612e0a250cb
Anirudh A V <anirudh.av02@gmail.com>
2022-09-29 23:20:50 +0530

    assets added

--- frontend-app/src/components/Navbar/Navbar.jsx
+++ frontend-app/src/components/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <div>
+        <img src={images.logo} alt='logo' />
+      </div>
+    </nav>
   )
 }
 
