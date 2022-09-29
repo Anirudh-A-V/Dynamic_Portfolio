c3d97c9b664be1e62f31584123828683afcb0d81
Anirudh A V <anirudh.av02@gmail.com>
2022-09-29 16:34:00 +0530

    Previous commit

---
+++ frontend-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './App.scss'
+
+import { About, Footer, Header, Skills, Testimonials, Work } from './containers';
+import { Navbar } from './components';
+
+const App = () => {
+  return (
+    <div className='app'>
+        <Navbar />
+        <Header />
+        <About />
+        <Skills />
+        <Work />
+        <Testimonials />
+        <Footer />
+
+    </div>
+  );
+}
+
+export default App
---
+++ frontend-app/src/containers/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Skills.scss'
+
+const Skills = () => {
+  return (
+    <div>
+      Skills
+    </div>
+  )
+}
+
+export default Skills
