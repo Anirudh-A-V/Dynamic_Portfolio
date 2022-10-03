099040671c327b6a2630e3accdf47c5637a8b1cb
Anirudh A V <anirudh.av02@gmail.com>
2022-10-03 22:01:21 +0530

    sanity integrated with About component > Working perfectly on my local machine

--- frontend-app/src/containers/About/About.jsx
+++ frontend-app/src/containers/About/About.jsx
@@ -4,14 +4,28 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './About.scss'
 
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
-  { title: 'Web Design', description: 'I am a good designer.', imgUrl: images.about02 },
-  { title: 'Web Animations', description: 'I am a good animator.', imgUrl: images.about03 },
-  { title: 'Web Animations', description: 'I am a good animator.', imgUrl: images.about04 }
-]
+import {urlFor, client} from '../../client';
+
+// const abouts = [
+//   { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01 },
+//   { title: 'Web Design', description: 'I am a good designer.', imgUrl: images.about02 },
+//   { title: 'Web Animations', description: 'I am a good animator.', imgUrl: images.about03 },
+//   { title: 'Web Animations', description: 'I am a good animator.', imgUrl: images.about04 }
+// ]
 
 const About = () => {
+
+  const [abouts, setAbout] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((res) => {
+      setAbout(res);
+    });
+
+  }, []);
+
   return (
     <>
       <h2 className='head-text'> I know that <span>Good Development</span> <br /> means <span>Good Business</span> </h2>
@@ -24,7 +38,7 @@ const About = () => {
             transition={{ duration: 0.5, type: 'tween' }}
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
