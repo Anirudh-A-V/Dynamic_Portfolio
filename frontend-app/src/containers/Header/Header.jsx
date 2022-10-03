1e5fb7262a9215533a2bced1f920da16a8bda075
Anirudh A V <anirudh.av02@gmail.com>
2022-10-03 15:00:10 +0530

    Header section complete > Page header complete > Profile photo needs to be changed > Better icons needed for react > Suggestion: Dark mode

--- frontend-app/src/containers/Header/Header.jsx
+++ frontend-app/src/containers/Header/Header.jsx
@@ -1,10 +1,73 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
+
 import './Header.scss'
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0,1],
+    opacity: [0,1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    }
+  }
+}
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div id="home" className="app__header app__flex">
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: '20px' }}>
+              <p className='p-text'>Hello, I am </p>
+              <h1 className='head-text'>Anirudh</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className='p-text'>Web Developer</p>
+            <p className='p-text'>UI/UX Designer</p>
+            <p className='p-text'>Freelancer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="header" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_background"
+          className="overlay_circle"
+        />
+
+      </motion.div>
+
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+        >
+          {[images.python, images.react, images.figma].map((circle, index) => (
+            <div className="circle-cmp app__flex" key={`circle-${index}`}>
+              <img src={circle} alt="circle" />
+            </div>
+          ))}
+
+      </motion.div>
     </div>
   )
 }
