2eeed3e01fbdfb4600b11ecd2e5a31b0cc1a0f9c
Anirudh A V <anirudh.av02@gmail.com>
2022-10-04 16:55:09 +0530

    Filter logic added

--- frontend/src/containers/Work/Work.jsx
+++ frontend/src/containers/Work/Work.jsx
@@ -24,13 +24,24 @@ const Work = () => {
 
 
   const handleWorkFilter = (item) => {
-
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+      if (item === 'All') {
+        setFilterWork(works)
+      } else {
+        const filter = works.filter((work) => work.tags.includes(item))
+        setFilterWork(filter)
+      }
+    }, 500);
   }
+  
   return (
     <>
       <h2 className='head-text'>My Creative <span>Portfolio</span> </h2>
 
-      <div className='app__work-filters'>
+      <div className='app__work-filter'>
         {['ReactJS', 'Django', 'Flutter', 'Python', 'Web App', 'All'].map((item, index) => (
           <div key={index} onClick={() => handleWorkFilter(item)} className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}>
             {item}
@@ -49,25 +60,25 @@ const Work = () => {
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transstion={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                 className="app__work-hover app__flex"
               >
                 <a href={work.projectLink} target='_blank' rel='noreferrer'>
-                  <motion.div 
-                  whileInView={{scale: [0, 1]}}
-                  whileHover={{ scale: [1, 0.9] }}
-                  transstion={{ duration: 0.25 }}
-                  className="app__flex"
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
                   >
                     <AiFillEye />
                   </motion.div>
                 </a>
                 <a href={work.codeLink} target='_blank' rel='noreferrer'>
-                  <motion.div 
-                  whileInView={{scale: [0, 1]}}
-                  whileHover={{ scale: [1, 0.9] }}
-                  transstion={{ duration: 0.25 }}
-                  className="app__flex"
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
                   >
                     <AiFillGithub />
                   </motion.div>
@@ -78,7 +89,7 @@ const Work = () => {
 
             <div className='app__work-content app__flex'>
               <h4 className='bold-text'>{work.title}</h4>
-              <p className='p-text' style={{ marginTop : 10}}>{work.description}</p>
+              <p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
 
               <div className='app__work-tag app__flex'>
                 <p className='p-text'>{work.tags[0]}</p>
