6cf94c00f30b6f7a4a15011fad84b81da25b6be2
Anirudh A V <anirudh.av02@gmail.com>
2022-10-04 12:48:45 +0530

    NavigationDots edit

--- frontend/src/components/NavigationDots.jsx
+++ frontend/src/components/NavigationDots.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 
 const NavigationDots = ({ active }) => {
-  return (
-    <div className="app__navigation">
-      {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map(
-        (item, index) => (
-          // <li key={`link-${item}`} >
-          <a 
-          href={`#${item}`} 
-          key={item+index}
-          className="app__navigation-dot"
-          style={active === item ? { backgroundColor: '#313BAC' } : {}}
-          onClick={() => setToggle(false)}
-          />
-          // </li>
-        ),
-      )}
-    </div>
-  )
+    return (
+        <div className="app__navigation">
+            {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map(
+                (item, index) => (
+                    <a
+                        href={`#${item}`}
+                        key={item + index}
+                        className="app__navigation-dot"
+                        style={active === item ? { backgroundColor: '#313BAC' } : {}}
+                        
+                    />
+                )
+            )}
+        </div>
+    )
 }
 
 export default NavigationDots
