b9b75a75a90f23b44005afb07ebd25837640e1c8
Anirudh A V <anirudh.av02@gmail.com>
2022-10-04 10:31:18 +0530

    Higher order component AppWrap configured

---
+++ frontend/src/wrapper/AppWrap.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavigationDots, SocialMedia } from '../components';
+
+const AppWrap = (Component, idName, classNames) => function HOC(){
+  return (
+    <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia/>
+
+        <div className="app__wrapper app__flex">
+            <Component/>
+        </div>
+
+        <div className='copyright'>
+            <p className='p-text'>©2022 ANIRUDH A V</p>
+            <p className='p-text'>All rights reserved</p>
+        </div>
+    </div>
+  )
+}
+
+export default AppWrap
