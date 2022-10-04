2e59505744d14295a549cb074c731401a2242e23
Anirudh A V <anirudh.av02@gmail.com>
2022-10-05 01:09:46 +0530

    Experience section added

--- frontend/src/containers/Skills/Skills.jsx
+++ frontend/src/containers/Skills/Skills.jsx
@@ -45,6 +45,44 @@ const Skills = () => {
           ))}
         </motion.div>
 
+        <motion.div className='app__skills-exp'>
+          {experience.map((exp) => (
+            <motion.div
+              className='app__skills-exp-item'
+              key={exp.year}
+            >
+              <div className='app__skills-exp-year'>
+                <p className='bold-text'>{exp.year}</p>
+              </div>
+              <motion.div className='app__skills-exp-works'>
+                {exp.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className='app__skills-exp-work app__flex'
+                      data-tip
+                      data-for={work.name}
+                      key={work.name}
+                    >
+                      <h4 className='bold-text'>{work.name}</h4>
+                      <p className='p-text'>{work.company}</p>
+                    </motion.div>
+                    <ReactTooltip
+                      id={work.name}
+                      place='right'
+                      effect='solid'
+                      arrowColor='#fff'
+                      className='skills-tooptip'
+                    >
+                      {work.desc}
+                    </ReactTooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
     </>
   )
