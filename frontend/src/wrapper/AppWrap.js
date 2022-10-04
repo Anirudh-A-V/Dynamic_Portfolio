47738696ae1fdf9bbc64249f724e506a7a2bbb63
Anirudh A V <anirudh.av02@gmail.com>
2022-10-04 10:33:26 +0530

    edit

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { NavigationDots, SocialMedia } from '../components';
 
-const AppWrap = (Component, idName, classNames) => function HOC(){
-  return (
-    <div id={idName} className={`app__container ${classNames}`}>
-        <SocialMedia/>
+const AppWrap = (Component, idName, classNames) => function HOC() {
+    return (
+        <div id={idName} className={`app__container ${classNames}`}>
+            <SocialMedia />
 
-        <div className="app__wrapper app__flex">
-            <Component/>
+            <div className="app__wrapper app__flex">
+                <Component />
+                <div className='copyright'>
+                    <p className='p-text'>©2022 ANIRUDH A V</p>
+                    <p className='p-text'>All rights reserved</p>
+                </div>
+            </div>
+            <NavigationDots active={idName} />
         </div>
-
-        <div className='copyright'>
-            <p className='p-text'>©2022 ANIRUDH A V</p>
-            <p className='p-text'>All rights reserved</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default AppWrap
