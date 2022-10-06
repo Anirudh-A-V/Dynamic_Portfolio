72c7ce03a6d2867f02e5241d64ec5c68ed1aad23
Anirudh A V <anirudh.av02@gmail.com>
2022-10-06 16:00:33 +0530

    nodejs added in header

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
         >
-          {[images.python, images.react, images.figma].map((circle, index) => (
+          {[images.python, images.react, images.node].map((circle, index) => (
             <div className="circle-cmp app__flex" key={`circle-${index}`}>
               <img src={circle} alt="circle" />
             </div>
