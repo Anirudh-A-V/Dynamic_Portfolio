cc4b95a589c89418088916f57e125e171f86660c
Anirudh A V <anirudh.av02@gmail.com>
2022-10-06 14:38:20 +0530

    SocialMedia links added

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        <div>
-            <BsTwitter/>
-        </div>
-        <div>
-            <BsInstagram/>
-        </div>
-        <div>
-            <FaFacebookF/>
-        </div>
+      <div>
+        <a href='https://www.linkedin.com/in/anirudh-a-v/' target='_blank' rel='noreferrer'>
+          <FaLinkedinIn />
+        </a>
+      </div>
+      <div>
+        <a href='https://twitter.com/anirudh_av02' target='_blank' rel='noreferrer'>
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href='https://www.instagram.com/a_rudh_av11_?r=nametag' target='_blank' rel='noreferrer'>
+          <BsInstagram />
+        </a>
+      </div>
     </div>
   )
 }
