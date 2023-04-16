2dbfa9d35a1feb5f59155aafe9088b959f3bc908
Anirudh A V <anirudh.av02@gmail.com>
2023-04-16 16:03:46 +0530

    feat: Google Analytics added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ReactGA from 'react-ga'
 
 import './App.scss'
 
 import { About, Footer, Header, Skills, Testimonials, Work } from './containers';
 import { Navbar } from './components';
 
+const trackingId = process.env.REACT_APP_GA_TRACKING_ID;
+ReactGA.initialize(trackingId);
+
 const App = () => {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className='app'>
-        <Navbar />
-        <Header />
-        <About />
-        <Work />
-        <Skills />
-        <Testimonials />
-        <Footer />
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
 
     </div>
   );
