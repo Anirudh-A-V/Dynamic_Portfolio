5bcb330654ef90270714eff725390b46e04af01a
Anirudh A V <anirudh.av02@gmail.com>
2023-04-18 14:45:32 +0530

    feat: Social media nav icons turned to fabs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.scss'
 
 import { About, Footer, Header, Skills, Testimonials, Work } from './containers';
 import { Navbar } from './components';
+import { SocialMedia } from './components';
 
 const trackingId = process.env.REACT_APP_GA_TRACKING_ID;
 ReactGA.initialize(trackingId);
@@ -28,6 +29,7 @@ const App = () => {
       <Skills />
       <Testimonials />
       <Footer />
+      <SocialMedia />
 
     </div>
   );
--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -4,7 +4,7 @@ import { NavigationDots, SocialMedia } from '../components';
 const AppWrap = (Component, idName, classNames) => function HOC() {
     return (
         <div id={idName} className={`app__container ${classNames}`}>
-            <SocialMedia />
+            {/* <SocialMedia /> */}
 
             <div className="app__wrapper app__flex">
                 <Component />
