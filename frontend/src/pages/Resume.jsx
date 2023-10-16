5e3a9cdb0d69481212fb265fa3e28631f987697e
Anirudh A V <anirudh.av02@gmail.com>
2023-10-16 18:42:52 +0530

    update: resume updated

--- frontend/src/pages/Resume.jsx
+++ frontend/src/pages/Resume.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 const Resume = () => {
     useEffect(() => {
-        window.location.replace('https://drive.google.com/file/d/1dqIoR91xWqPJutwbsUDpWTxhuS6dBIbH/view?usp=sharing');
+        window.location.replace('https://drive.google.com/file/d/1vqHnh7LIyvCGPGi2hkTnXlrd_dP_dYSS/view?usp=sharing');
     }, []);
 
     return null;
